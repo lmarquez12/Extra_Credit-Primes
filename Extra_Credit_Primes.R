0f06cd23aa31fc7f03e273d716d0166bf25eac62
lmarquez12 <lmarquez12@students.avc.edu>
2024-07-31 03:37:46 +0000

    commit

--- Extra_Credit_Primes.R
+++ Extra_Credit_Primes.R
@@ -1,3 +1,15 @@
 Title:"Extra_Credit-Primes"
 Author:"Lizeth Marquez"
 Date:"30 July 2024"
+
+
+```{r}
+is_prime <- function(n) {if (n <= 1) return(FALSE)
+  for (i in 2:sqrt(n)) {if (n %% i == 0) return(FALSE)}
+  return(TRUE)}
+
+prime_numbers <- which(sapply(1:1000, is_prime))
+
+print(prime_numbers)
+```
+
